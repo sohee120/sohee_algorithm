9fcc5d1a00aab400d1c02a18aff318bb24eb2930
sohee120 <yuhsohee120@gmail.com>
2022-01-19 13:47:23 +0900

    Add: study algorithm

---
+++ BaekJoon/BaekJoon8393/BaekJoon8393/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  BaekJoon8393
+//
+//  Created by 윤소희 on 2022/01/19.
+//
+
+import Foundation
+
+let num = Int(readLine()!)!
+var sum = 0
+
+for i in 1...num {
+    
+    sum += i
+    
+}
+
+print ( sum )
