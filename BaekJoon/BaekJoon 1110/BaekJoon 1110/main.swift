68bc55b1b1cdc490cd3bf2f2c43f93b74e5daaef
sohee120 <yuhsohee120@gmail.com>
2022-01-25 16:29:20 +0900

    Add: study algorithm

--- BaekJoon/BaekJoon 1110/BaekJoon 1110/main.swift
+++ BaekJoon/BaekJoon 1110/BaekJoon 1110/main.swift
@@ -1,7 +1,37 @@
 //
 //  main.swift
 
-
 import Foundation
+
+var count = 1
+var input = Int(readLine()!)!
+
+    if input < 10 {
+        input *= 10
+    }
+
+var Num = input
+
+while true {
+
+    let a = Int(Num) / 10
+    let b = Int(Num) % 10
+
+
+    let c = a + b
+    
+    let newNum = 10 * b + (c % 10)
+    
+    if input == newNum {
+        break
+    }
+    
+    Num = newNum
+    
+    count += 1
+
+}
+
+print(count)
 
 
