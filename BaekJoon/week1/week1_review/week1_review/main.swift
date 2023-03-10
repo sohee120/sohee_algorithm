33823a87a737ce2cddd544103cdb06a9778cfbf8
sohee120 <yuhsohee120@gmail.com>
2023-03-10 17:00:56 +0900

    Add: inflearn week1 review

--- BaekJoon/week1/week1_review/week1_review/main.swift
+++ BaekJoon/week1/week1_review/week1_review/main.swift
@@ -95,27 +95,110 @@ import Foundation
 //}
 
 //MARK: - 1159
-var cnt = [Character:Int]()
-var answer = ""
-var num = Int(readLine()!)!
+//var cnt = [Character:Int]()
+//var answer = ""
+//var num = Int(readLine()!)!
+//
+//for _ in 0..<num {
+//    let firstName = readLine()!.first!
+//    if cnt[firstName] == nil {
+//        cnt[firstName] = 1
+//    }else{
+//        cnt[firstName]! += 1
+//    }
+//}
+//
+//for (key, value) in cnt.sorted(by: <) {
+//    if value >= 5 {
+//        answer += String(key)
+//    }
+//}
+//
+//if answer.isEmpty {
+//    answer = "PREDAJA"
+//}
+//
+//print(answer)
 
-for _ in 0..<num {
-    let firstName = readLine()!.first!
-    if cnt[firstName] == nil {
-        cnt[firstName] = 1
-    }else{
-        cnt[firstName]! += 1
-    }
-}
+//MARK: - 2309
+//func combination(array: [Int], length: Int) -> [[Int]] {
+//    var result = [[Int]]()
+//
+//    func combi(nowIndex index: Int, now: [Int]) {
+//        if now.count == length {
+//            result.append(now)
+//            return
+//        }
+//
+//        for i in index..<array.count {
+//            combi(nowIndex: i+1, now: now + [array[i]] )
+//        }
+//    }
+//    combi(nowIndex: 0, now: [])
+//    return result
+//}
+//
+//var arr = [Int]()
+//
+//for _ in 0..<9 {
+//    let input = Int(readLine()!)!
+//    arr.append(input)
+//}
+//
+//let combinations = combination(array: arr, length: 7)
+//
+//for combination in combinations {
+//    if combination.reduce(0, +) == 100 {
+//        combination.sorted().forEach { height in
+//            print(height)
+//        }
+//        break
+//    }
+//}
 
-for (key, value) in cnt.sorted(by: <) {
-    if value >= 5 {
-        answer += String(key)
-    }
-}
+//MARK: - 9375
+//var t = Int(readLine()!)!
+//
+//while t > 0 {
+//    var clothes = [String:Int]()
+//    var n = Int(readLine()!)!
+//    var answer = 1
+//
+//    for _ in 0..<n {
+//        let input = readLine()!.components(separatedBy: " ")
+//        if clothes[input[1]] == nil {
+//            clothes[input[1]] = 1
+//        }else{
+//            clothes[input[1]]! += 1
+//        }
+//    }
+//    for (_, value) in clothes {
+//        answer *= value + 1
+//    }
+//    t -= 1
+//    print(answer-1)
+//}
 
-if answer.isEmpty {
-    answer = "PREDAJA"
-}
+//MARK: - 11655
+//let input = readLine()!
+//var answer = ""
+//
+//for i in input {
+//    var ascii = i.asciiValue!
+//    if ascii >= 65 && ascii <= 77 {
+//        ascii += 13
+//    }else if ascii >= 78 && ascii <= 90 {
+//        ascii -= 13
+//    }else if ascii >= 97 && ascii <= 109 {
+//        ascii += 13
+//    }else if ascii >= 110 && ascii <= 123 {
+//        ascii -= 13
+//    }
+//    answer += "\(UnicodeScalar(ascii))"
+//}
+//
+//print(answer)
 
-print(answer)
+
+
+
