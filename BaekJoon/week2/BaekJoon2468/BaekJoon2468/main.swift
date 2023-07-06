3f8997c21e4ae3097d617184b8a673e4fd4d2ada
sohee120 <yuhsohee120@gmail.com>
2023-07-06 14:27:29 +0900

    inflearn week2 BaekJoon2468

--- BaekJoon/week2/BaekJoon2468/BaekJoon2468/main.swift
+++ BaekJoon/week2/BaekJoon2468/BaekJoon2468/main.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 
+
 let n = Int(readLine()!)!
 var area = Array(repeating: Array(repeating: 0, count: n), count: n)
 var cnt = 0
-var cntList = [Int]()
+var visited = Array(repeating: Array(repeating: false, count: n), count: n)
+var heights = Set<Int>()
 
 let dy = [1, 0, -1, 0]
 let dx = [0, 1, 0, -1]
@@ -18,42 +20,60 @@ let dx = [0, 1, 0, -1]
 for i in 0..<n {
     let height = readLine()!.split(separator: " ").map{Int(String($0))!}
     area[i] = height
+    for j in height {
+        heights.insert(j)
+    }
 }
 
-func bfs(_ i: Int, _ j:Int) -> Int {
+func bfs(_ i: Int, _ j:Int) {
+    visited[i][j] = true
     var queue = [(Int, Int)]()
     queue.append((i,j))
-    area[i][j] = n - 1
-    cnt = 1
-    
+
     while !queue.isEmpty {
-        let dequeue = queue.removeFirst()
+        let dequeue = queue.removeLast()
         let y = dequeue.0
         let x = dequeue.1
-        
+
         for i in 0..<4 {
             let ny = y + dy[i]
             let nx = x + dx[i]
-            
-            if ny < 0 || ny >= n || nx < 0 || nx >= n || area[ny][nx] <= n {
+
+            if ny < 0 || ny >= n || nx < 0 || nx >= n || visited[ny][nx] {
                 continue
             }
             queue.append((ny, nx))
-            area[ny][nx] = n - 1
-            cnt += 1
+            visited[ny][nx] = true
+        }
+    }
+}
+
+func bfsAll() -> Int {
+    var cnt = 0
+    for i in 0..<n {
+        for j in 0..<n {
+            if(!visited[i][j]){
+                bfs(i, j)
+                cnt += 1
+            }
         }
     }
     return cnt
 }
 
-for i in 0..<n {
-    for j in 0..<n {
-        if area[i][j] > n {
-            cntList.append(bfs(i,j))
+let maxHeight = heights.max()
+var safeAreaNum = 0
+
+for k in 1...maxHeight! {
+    for i in 0..<n {
+        for j in 0..<n {
+            if area[i][j] <= k {
+                visited[i][j] = true
+            }
         }
     }
+    safeAreaNum = max(bfsAll(), safeAreaNum)
+    visited = Array(repeating: Array(repeating: false, count: n), count: n)
 }
 
-
-print(cntList.count)
-print(cntList)
+print(safeAreaNum)
