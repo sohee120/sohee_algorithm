cbb91ca3752a64c39c485cb34cddeddf909d9a05
sohee120 <yuhsohee120@gmail.com>
2024-01-21 19:45:22 +0900

    review_baekjoon2583

--- BaekJoon/BaekJoon_2583/BaekJoon_2583/main.swift
+++ BaekJoon/BaekJoon_2583/BaekJoon_2583/main.swift
@@ -1,20 +1,12 @@
-//
-//  main.swift
-//  BaekJoon_2583
-//
-//  Created by 윤소희 on 2024/01/18.
-//
-
 import Foundation
 
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let m = input[0], n = input[1]
 var k = input[2]
-
 var paper = Array(repeating: Array(repeating: 0, count: n), count: m)
+var visited = Array(repeating: Array(repeating: false, count: n), count: m)
 let dy = [1, 0, -1, 0]
 let dx = [0, 1, 0, -1]
-var cnt = 0
 var cntList = [Int]()
 
 while k>0 {
@@ -28,41 +20,41 @@ while k>0 {
     k -= 1
 }
 
-func bfs(_ i:Int, _ j: Int) -> Int {
-  var queue = [(Int, Int)]()
+func bfs(_ i: Int, _ j: Int) -> Int {
+    var cnt = 0
+    var queue = [(Int, Int)]()
     queue.append((i,j))
-    paper[i][j] = 1
-    cnt = 1
+    visited[i][j] = true
+    cnt += 1
     
     while !queue.isEmpty {
-        let dequeue = queue.removeFirst()
-        let y = dequeue.0
-        let x = dequeue.1
+        let dequeu = queue.removeFirst()
+        let y = dequeu.0
+        let x = dequeu.1
         
         for i in 0..<4 {
             let ny = y + dy[i]
             let nx = x + dx[i]
             
-            if ny < 0 || ny >= m || nx < 0 || nx >= n {
+            if ny < 0 || nx < 0 || ny >= m || nx >= n || visited[ny][nx] || paper[ny][nx] == 1 {
                 continue
             }
-            if paper[ny][nx] == 0 {
-                paper[ny][nx] = 1
-                queue.append((ny,nx))
-                cnt += 1
-            }
+            queue.append((ny, nx))
+            visited[ny][nx] = true
+            cnt += 1
         }
     }
     return cnt
 }
 
-for i in 0..<m {
-    for j in 0..<n {
-        if paper[i][j] == 0 {
-            cntList.append(bfs(i,j))
+for y in 0..<m {
+    for x in 0..<n {
+        if !visited[y][x] && paper[y][x] == 0 {
+            cntList.append(bfs(y, x))
         }
     }
 }
 
 print(cntList.count)
 print(cntList.sorted().map{String($0)}.joined(separator: " "))
+
