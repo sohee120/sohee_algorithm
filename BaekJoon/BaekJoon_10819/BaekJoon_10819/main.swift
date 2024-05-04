9d05a113aa9b9f4f0bff4222f465e6afddcda18d
sohee120 <yuhsohee120@gmail.com>
2024-05-04 17:54:20 +0900

    refactor: 백준 10819 코드 수정

--- BaekJoon/BaekJoon_10819/BaekJoon_10819/main.swift
+++ BaekJoon/BaekJoon_10819/BaekJoon_10819/main.swift
@@ -11,7 +11,7 @@ let n = Int(readLine()!)!
 var numbers = readLine()!.split(separator: " ").map { Int(String($0))! }
 var maxValue = 0
 
-func diffSum(_ arr: [Int]) -> Int {
+func calcSum(_ arr: [Int]) -> Int {
     var sum = 0
     for idx in 0..<arr.count-1 {
         sum += abs(arr[idx] - arr[idx+1])
@@ -19,24 +19,23 @@ func diffSum(_ arr: [Int]) -> Int {
     return sum
 }
 
-func permutation(_ arr: inout [Int], _ curr: Int) {
-    guard curr < n else {
-        let sum = diffSum(arr)
-        if sum > maxValue {
-            maxValue = sum
-        }
+func permutation(_ arr: [Int], _ depth: Int) {
+    var nums = arr
+    guard depth < n else {
+        let sum = calcSum(arr)
+        maxValue = max(sum, maxValue)
         return
     }
     
-    for changing in curr..<n {
-        arr.swapAt(curr, changing)
-        permutation(&arr, curr+1)
-        arr.swapAt(curr, changing)
+    for i in depth..<n {
+        nums.swapAt(depth, i)
+        permutation(nums, depth+1)
+        nums.swapAt(depth, i)
     }
 }
 
 
-permutation(&numbers, 0)
+permutation(numbers, 0)
 print(maxValue)
 
 
---
+++ BaekJoon/BaekJoon_7490/BaekJoon_7490/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  BaekJoon_7490
+//
+//  Created by 윤소희 on 5/4/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
