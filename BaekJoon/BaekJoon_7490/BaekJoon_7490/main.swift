91c142f44ec92128daff5fa26289188319eef96c
sohee120 <yuhsohee120@gmail.com>
2024-05-05 23:07:11 +0900

    BaekJoon 7490

--- BaekJoon/BaekJoon_7490/BaekJoon_7490/main.swift
+++ BaekJoon/BaekJoon_7490/BaekJoon_7490/main.swift
@@ -7,5 +7,36 @@
 
 import Foundation
 
-print("Hello, World!")
+var ans = [String]()
+
+func solve(target: Int, sum: Int, sign: Int, prev: Int, idx: Int, expression: String) {
+    if idx == target {
+        let newSum = sum + (prev * sign)
+        if newSum == 0 {
+            ans.append(expression)
+        }
+        return
+    } else {
+        solve(target: target, sum: sum + (prev * sign), sign: 1, prev: idx + 1, idx: idx + 1, expression: expression + "+" + String(idx + 1))
+        solve(target: target, sum: sum + (prev * sign), sign: -1, prev: idx + 1, idx: idx + 1, expression: expression + "-" + String(idx + 1))
+        solve(target: target, sum: sum, sign: sign, prev: prev * 10 + (idx + 1), idx: idx + 1, expression: expression + " " + String(idx + 1))
+    }
+}
+
+var testCaseCount: Int = Int(readLine()!)!
+
+while testCaseCount > 0 {
+    testCaseCount -= 1
+    let num = Int(readLine()!)!
+
+    solve(target: num, sum: 0, sign: 1, prev: 1, idx: 1, expression: "1")
+
+    ans.sort()
+    for expression in ans {
+        print(expression)
+    }
+    print()
+    ans.removeAll()
+}
+
 
