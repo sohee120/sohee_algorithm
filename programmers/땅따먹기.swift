237c7be61d3cca5ee5d1700a732c9897d00da913
sohee120 <yuhsohee120@gmail.com>
2024-06-05 00:39:26 +0900

    programmers 땅따먹기

---
+++ programmers/땅따먹기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ land:[[Int]]) -> Int{
+    var answer = 0
+    var land = land
+    for i in 0..<(land.count-1) {
+        land[i+1][0] += max(land[i][1], land[i][2], land[i][3])
+        land[i+1][1] += max(land[i][0], land[i][2], land[i][3])
+        land[i+1][2] += max(land[i][0], land[i][1], land[i][3])
+        land[i+1][3] += max(land[i][0], land[i][1], land[i][2])
+    }
+ 
+    return land[land.count-1].max()!
+}
